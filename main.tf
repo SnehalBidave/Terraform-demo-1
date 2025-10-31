e76286634c3b623baf6662a5656ef8e571fad507
SnehalBidave <snehalkb7@gmail.com>
2025-10-31 16:03:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,10 +84,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
 }
 
 resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
 
   bucket = aws_s3_bucket.example.id
   acl    = "public-read"
